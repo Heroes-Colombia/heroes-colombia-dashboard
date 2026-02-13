20989c60f9a1d5bc78c1c5b1eaa60378c759f657
Jonathan Gonzalez <jonathan.gonzalez.botero@gmail.com>
2026-02-13 23:36:09 +1000

    include chat widget for users to interact with AI

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,19 @@ export default function RootLayout({
           <Suspense fallback={null}>{children}</Suspense>
         </AuthProvider>
         <Analytics />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function() {
+            var script = document.createElement('script');
+            script.dataset.bot = "698eee354c9ee63bf5cf99ac";
+            script.dataset.zindex = "99999";
+            script.src = "https://panel.chatfuel.com/widgets/chat-widget/chat-widget.js";
+            script.async = true;
+            script.defer = true;
+            document.getElementsByTagName('head')[0].appendChild(script);
+          })()`,
+          }}
+        />
       </body>
     </html>
   )
